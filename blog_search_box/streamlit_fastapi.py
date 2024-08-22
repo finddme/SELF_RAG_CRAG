827bef3b3fe67361fa31b708fada7437401acf6a
finddme <53667002+finddme@users.noreply.github.com>
2024-08-22 11:47:04 +0900

    Update streamlit_fastapi.py

--- blog_search_box/streamlit_fastapi.py
+++ blog_search_box/streamlit_fastapi.py
@@ -50,79 +50,79 @@ def process_sources(sources):
 # st.sidebar.button('Clear History', on_click=clear_chat_history)
 # async def run_convo():
 def run_convo():
-    with st.form(key="form"):
-        # st.title(""":blue[Search / Chat with finddme]""")
-        # st.title(""":blue[RAG 기반 블로그 검색창]""")
-        st.markdown("""
-        <style>
-        .custom-font {
-            font-weight: bold;
-            font-size:28px !important;
-            color: #0067C8;
-            margin-top: 0%;
-            font-family: Arial, Helvetica, sans-serif;
-        }
-        </style>
-        """, unsafe_allow_html=True)
-        st.markdown("""
-        <style>
-        .custom-font2 {
-            # font-size:12px !important;
-            color: #D97757;
-            # margin-top: 0%;
-            # font-family: Arial, Helvetica, sans-serif;
-        }
-        </style>
-        """, unsafe_allow_html=True)
-        # st.markdown("""<p class='custom-font'>😎 RAG 기반 블로그 검색창</p>""", unsafe_allow_html=True )
+    # with st.form(key="form"):
+    # st.title(""":blue[Search / Chat with finddme]""")
+    # st.title(""":blue[RAG 기반 블로그 검색창]""")
+    st.markdown("""
+    <style>
+    .custom-font {
+        font-weight: bold;
+        font-size:28px !important;
+        color: #0067C8;
+        margin-top: 0%;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+    st.markdown("""
+    <style>
+    .custom-font2 {
+        # font-size:12px !important;
+        color: #D97757;
+        # margin-top: 0%;
+        # font-family: Arial, Helvetica, sans-serif;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+    # st.markdown("""<p class='custom-font'>😎 RAG 기반 블로그 검색창</p>""", unsafe_allow_html=True )
+
+    # st.markdown("""<p class='custom-font2'>3 pipeline (1)LLM/NLP/AI->블로그 포스트 RAG (2)일반 질문->Web search RAG (3)일상 대화->일반 chatbot</p>""", 
+    # :orange[*Query Router | Hallucination Grader | Reranker | Web search*]\\
+    #             unsafe_allow_html=True 
+    #             )
+    user_input = st.text_input("""검색어를 입력하세요.\\
+                                Route the query into 3 pipelines : 
+                                :orange[**Blog post retrieval / Web search / Casual chat**]
+                                """)
+    # submitted = st.form_submit_button('Send')
     
-        # st.markdown("""<p class='custom-font2'>3 pipeline (1)LLM/NLP/AI->블로그 포스트 RAG (2)일반 질문->Web search RAG (3)일상 대화->일반 chatbot</p>""", 
-        # :orange[*Query Router | Hallucination Grader | Reranker | Web search*]\\
-        #             unsafe_allow_html=True 
-        #             )
-        user_input = st.text_input("""검색어를 입력하세요.\\
-                                    Route the query into 3 pipelines : 
-                                    :orange[**Blog post retrieval / Web search / Casual chat**]
-                                    """)
-        # submitted = st.form_submit_button('Send')
+        # submit = st.form_submit_button(label="Submit")
+        # if submit:
+        #     with st.spinner("Generating response..."):
+        #         try:
+        #             answer, source = get_response(user_input)
+        #             answer=answer+"\n"
+        #             source=process_sources(source)
+        #             if type(source)==list:
+        #                 answer=answer+"[Blog post retrieval]\n"
+        #                 answer=reduce(lambda acc, s: acc + s + "\n", source, answer)
+        #             else:
+        #                 answer=answer+source
+        #             st.write(str(answer))
+        #         except Exception as e:
+        #             error_msg="Need to check port-forwarding status: Need to verify the server running FastAPI (port 7808)"
+        #             st.error(f"Error: {error_msg}")
     
-        submit = st.form_submit_button(label="Submit")
-        if submit:
-            with st.spinner("Generating response..."):
-                try:
-                    answer, source = get_response(user_input)
-                    answer=answer+"\n"
-                    source=process_sources(source)
-                    if type(source)==list:
-                        answer=answer+"[Blog post retrieval]\n"
-                        answer=reduce(lambda acc, s: acc + s + "\n", source, answer)
-                    else:
-                        answer=answer+source
-                    st.write(str(answer))
-                except Exception as e:
-                    error_msg="Need to check port-forwarding status: Need to verify the server running FastAPI (port 7808)"
-                    st.error(f"Error: {error_msg}")
-    
-    # if user_input:
-    #     with st.spinner("Generating response..."):
-    #         try:
-    #             # answer, source = await get_response(user_input)
-    #             answer, source = get_response(user_input)
-    #             # source= [s["source_title"].split("\n")[0] if s["source_title"] != " " else " " for s in source]
-    #             # if source[0] !=" ":
-    #             #     answer+= "\n\nsources->\n"+"\n".join(source)
-    #             answer=answer+"\n"
-    #             source=process_sources(source)
-    #             if type(source)==list:
-    #                 answer=answer+"[Blog post retrieval]\n"
-    #                 answer=reduce(lambda acc, s: acc + s + "\n", source, answer)
-    #             else:
-    #                 answer=answer+source
-    #             st.write(str(answer))
-    #         except Exception as e:
-    #             error_msg="Need to check port-forwarding status: Need to verify the server running FastAPI (port 7808)"
-    #             st.error(f"Error: {error_msg}")
-    #             # st.error(f"Error: {e}")
+    if user_input:
+        with st.spinner("Generating response..."):
+            try:
+                # answer, source = await get_response(user_input)
+                answer, source = get_response(user_input)
+                # source= [s["source_title"].split("\n")[0] if s["source_title"] != " " else " " for s in source]
+                # if source[0] !=" ":
+                #     answer+= "\n\nsources->\n"+"\n".join(source)
+                answer=str(answer)+"\n"
+                source=process_sources(source)
+                if type(source)==list:
+                    answer=answer+"[Blog post retrieval]\n"
+                    answer=reduce(lambda acc, s: acc + s + "\n", source, answer)
+                else:
+                    answer=answer+source
+                st.write(str(answer))
+            except Exception as e:
+                error_msg="Need to check port-forwarding status: Need to verify the server running FastAPI (port 7808)"
+                st.error(f"Error: {error_msg}")
+                # st.error(f"Error: {e}")
     
 # ===============================================================================================================
 
