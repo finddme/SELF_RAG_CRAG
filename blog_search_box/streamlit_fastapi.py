9726a88537d9a09b469550735e5b8be52fab0a2b
finddme <53667002+finddme@users.noreply.github.com>
2024-08-27 18:31:29 +0900

    Update streamlit_fastapi.py

--- blog_search_box/streamlit_fastapi.py
+++ blog_search_box/streamlit_fastapi.py
@@ -104,7 +104,7 @@ def run_convo():
         #         except Exception as e:
         #             error_msg="Need to check port-forwarding status: Need to verify the server running FastAPI (port 7808)"
         #             st.error(f"Error: {error_msg}")
-    st.image("https://finddme.github.io/public/searchbox1.png", caption="search box pipeline")
+    
     if user_input:
         with st.spinner("Generating response..."):
             try:
@@ -127,6 +127,7 @@ def run_convo():
                 error_msg="Need to check port-forwarding status: Need to verify the server running FastAPI (port 7808)"
                 st.error(f"Error: {error_msg}")
                 # st.error(f"Error: {e}")
+    st.image("https://finddme.github.io/public/searchbox1.png", caption="search box pipeline")
     
 # ===============================================================================================================
 
