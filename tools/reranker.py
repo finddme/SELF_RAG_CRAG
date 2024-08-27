eb6a1c06551ba9d754f5b9b8a89905aa7e9c22bd
finddme <53667002+finddme@users.noreply.github.com>
2024-08-27 18:25:09 +0900

    Update reranker.py

--- tools/reranker.py
+++ tools/reranker.py
@@ -8,12 +8,12 @@ coher_API_KEY = ""
 def reranker_cohere(state,co,weaviate_class):
     print("--- RERANK ---")
     question = state["question"]
-    documents_or = state["documents"]
+    documents_co = state["documents"]
     # weaviate_class=weaviate_class.title()
     weaviate_class=weaviate_class.capitalize()
     
-    documents=[r["text"] for r in documents_or["data"]["Get"][weaviate_class]]
-    source=[{"source_title":r["source_title"],"source":r["source"]} for r in documents_or["data"]["Get"][weaviate_class]]
+    documents=[r["text"] for r in documents_co["data"]["Get"][weaviate_class]]
+    source=[{"source_title":r["source_title"],"source":r["source"]} for r in documents_co["data"]["Get"][weaviate_class]]
     
     rerank_res = co.rerank(
         model="rerank-multilingual-v3.0",
